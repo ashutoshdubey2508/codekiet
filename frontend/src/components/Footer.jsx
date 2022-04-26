f5840f5b574ecd759d1bd7d92ea9fcf2ac436f36
ashutoshdubey2508 <ashutoshdubey2508@gmail.com>
2022-04-26 16:06:37 +0530

    ui

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer=()=> {
+    return (
+        <div>
+           <span >Copyright &copy; Ashutosh Dubey</span>
+        </div>
+    );
+}
+
+export default Footer;
